20de47eea0e21e84a623f5a6c9f6ae2ccd77fe75
Bret Ikehara <bret@coverhound.com>
2016-11-11 15:41:08 -0800

    use gulp-git for postpublish task

--- Gulpfile.js
+++ Gulpfile.js
@@ -198,14 +198,27 @@ gulp.task('lint', () => {
 });
 
 
-gulp.task('postpublish', function () {
-  return gulp
-    .src('./package.json')
-    .pipe($.tagVersion())
-    .pipe($.gitPush({
-      repository: 'origin',
-      refspec: 'HEAD'
-    }));
+gulp.task('postpublish', function (done) {
+  const pkg = require('./package.json');
+  const version = `v${ pkg.version }`;
+  const message = `Release ${ version }`;
+  gulp.src('./package.json')
+    .pipe($.git.commit(message))
+    .on('end', function () {
+      $.git.tag(version, message, function (err) {
+        if (err) {
+          $.util.log($.util.colors.red(err));
+          done();
+        } else {
+          $.git.push('origin', 'master', { args: " --tags" }, function (err) {
+            if (err) {
+              $.util.log($.util.colors.red(err));
+            }
+            done();
+          });
+        }
+      });
+    });
 });
 
 gulp.task('default', ['build', 'examples', 'test:unit', 'test:e2e']);
